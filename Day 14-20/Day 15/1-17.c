0ce539fc33ff2eee58a32253c56a55b4fb925c80
100daysofimp <57603181+100daysofimp@users.noreply.github.com>
2019-12-23 08:02:45 -0700

    Day 15

---
+++ Day 14-20/Day 15/1-17.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/*Exercise 1-17: Write a program to print al input lines that are longer than 80 characters*/
+
+#define MAXLINE 1000		/*maximum input line size*/
+#define LONGLINE 80
+
+int getline(char[], int maxline);
+void copy(char from[], char to[]);
+
+/*print lines longer than LONGLINE*/
+main()
+{
+	int len;				/*Current line length*/
+	char line[MAXLINE];		/*Current input line*/
+	
+	while ((len = getline(line, MAXLINE)) > 0)
+		if (len > LONGLINE)
+			printf("%s \n", line);
+	
+	return 0;
+}
+int getline(char s[], int lim)
+{
+	int c, i, j;
+
+	j = 0;
+	for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i)
+		if (i < lim - 2) {
+			s[j] = c;  /*line still within boundaries*/
+			++j;
+		}
+	if (c == '\n') {
+		s[j] = c;
+		++j;
+		++i;
+	}
+	s[j] = '\0';
+	return i;
+}
